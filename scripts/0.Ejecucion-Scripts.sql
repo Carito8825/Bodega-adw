985da1a4c0226226e8c9ff38fd3d79086030b061
Carito8825 Carolina <caris_940115@hotmail.com>
2023-10-09 17:48:49 +0000

    comentario

---
+++ scripts/0.Ejecucion-Scripts.sql
@@ -0,0 +1,36 @@
+-- Creación de la Bodega de Datos
+
+source /scripts/1.Creacion-Bodega.sql
+
+-- Carga Dimension tiempo
+
+source /scripts/2.Carga-Dim_Tiempo.sql
+
+-- Carga Dimension Store
+
+source /scripts/3.Carga-Dim_Store.sql
+
+-- Carga Dimension Category
+
+source /scripts/4.Carga-Dim_category.sql
+
+-- Carga Dimension SubCategory
+
+source /scripts/5.Carga-Dim_subcategory.sql
+
+-- Carga Dimension Product
+
+source /scripts/6.Carga-Dim_product.sql
+
+-- Carga Fact Ventas
+
+source /scripts/7.Carga-Fact_Ventas.sql
+
+-- Consulta Bodega de Datos
+
+source /scripts/8.Consulta.sql
+
+-------------------------
+
+
+
